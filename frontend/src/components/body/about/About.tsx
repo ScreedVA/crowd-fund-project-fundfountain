f9a2e39b0c61f0f8ecb2f02fc93732a81d9e7f47
Christian Damete-Yeboa <christiandameteyeboa@gmail.com>
2024-10-25 20:59:31 +0200

    minimal project setup

--- frontend/src/components/body/about/About.tsx
+++ frontend/src/components/body/about/About.tsx
@@ -1,19 +1,7 @@
 import "./About.css";
-import photo from "./profile.jpg";
 
 function About() {
-  return (
-    <>
-      <img id="first" src={photo} alt="Profile" />
-      <div className="center-cointener ">
-        <h1 className="aline"> About Funding Projects!</h1>
-        <p className="aline">
-          In this page you will find projects to fund and people that will help
-          you!
-        </p>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default About;
--- frontend/src/components/body/contact/Contact.tsx
+++ frontend/src/components/body/contact/Contact.tsx
@@ -1,24 +1,7 @@
 import "./Contact.css";
-import { useContext, useState } from "react";
-import ListGroup from "../../ListProjects/ListProjects";
 
 function Contact() {
-  const [lastName, setLastName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const items = ["Project 1", "Project 2", "Contacto 2"];
-  return (
-    <>
-      <p>Contact</p>
-      <div className="center-cointener">
-        <ListGroup
-          items={items}
-          heading="Want to contact us?"
-          onSelectItem={() => console.log("something")}
-        ></ListGroup>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default Contact;
